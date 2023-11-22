53d303da2490d73c584a4eed8048d0d96001801e
Ziad Qouri <zqouri@e1r7p3.1337.ma>
2023-11-22 15:54:58 +0100

    Done

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: zqouri <zqouri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 01:59:00 by zqouri            #+#    #+#             */
-/*   Updated: 2023/11/20 23:53:24 by zqouri           ###   ########.fr       */
+/*   Updated: 2023/11/22 15:52:51 by zqouri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ static void	ft_next_char(char c, va_list args, int *size)
 	if (c == 'c')
 		ft_putchar(va_arg(args, int), size);
 	else if (c == 's')
-	{
 		ft_putstr(va_arg(args, char *), size);
-	}
 	else if (c == 'd')
 		ft_putnbr(va_arg(args, int), size);
 	else if (c == 'i')
@@ -34,6 +32,8 @@ static void	ft_next_char(char c, va_list args, int *size)
 		ft_puthex_upp(va_arg(args, unsigned int), size);
 	else if (c == '%')
 		ft_putchar('%', size);
+	else
+		ft_putchar(c, size);
 }
 
 int	ft_printf(const char *s, ...)
@@ -63,3 +63,15 @@ int	ft_printf(const char *s, ...)
 	va_end(args);
 	return (size);
 }
+
+
+//int main(void)
+//{
+//	ft_printf("%%%**************\n");
+//	printf("%%%***********\n");
+//	//ft_printf("%%%$$$$$$$$$$$$$$$\n");
+//	//printf("%d\n",ft_printf("%%%$$$$$$$$$$$$$$$"));
+//	//printf("%d\n",printf("%%%$$$$$$$$$$$$$$$"));
+////	printf("%d\n",ft_printf("%%%$$$"));
+////	printf("%d\n",printf("%%%$$$"));
+//}
